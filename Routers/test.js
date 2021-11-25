b7e4ad0a75124d6eb865c3af4ec54023142a2648
unknown <mohamedmujtaba0210@gmail.com>
2021-11-25 14:48:16 -0800

    bug

--- Routers/test.js
+++ Routers/test.js
@@ -3,56 +3,56 @@ const Test = require('../Schema/Test');
 
 
 //  Create Test
-router.post('/', async(req,res)=>{
+router.post('/', async (req, res) => {
   const newTest = new Test({
     testName: req.body.testName,
     normal: req.body.normal,
     price: req.body.price,
     comments: req.body.comments
   });
-  try{
+  try {
     const savedTest = await newTest.save();
     res.status(200).json(savedTest);
-  }catch(err){
-    res.status(400).json('User Has not Been Created');
+  } catch (err) {
+    res.status(400).json('Test Has not Been Created');
   }
 })
 
 
 // Get All Tests
-router.get('/',async(req,res)=>{
-  try{
+router.get('/', async (req, res) => {
+  try {
     const tests = await Test.find();
     res.status(200).json(tests);
-  }catch(err){
-    res.status(400).json('Something Whent Wrong');
+  } catch (err) {
+    res.status(400).json('Something Went Wrong');
   }
 })
 
 
 // Get One Test
-router.get('/:id', async(req,res)=>{
-  try{
+router.get('/:id', async (req, res) => {
+  try {
     const test = await Test.findById(req.params.id);
     res.status(200).json(test);
-  }catch(err){
+  } catch (err) {
     res.status(404).json('Test Not Found');
   }
 })
 
 
 // Update A Test 
-router.put('/:id',async (req,res)=>{
-  try{
+router.put('/:id', async (req, res) => {
+  try {
     const updatedUser = await Test.findByIdAndUpdate(
       req.params.id,
       {
-      $set:req.body
+        $set: req.body
       },
-      {new:true}
+      { new: true }
     );
     res.status(200).json(updatedUser);
-  }catch(err){
+  } catch (err) {
     res.status(400).json('Test Has Not Been Updated');
   }
 })
